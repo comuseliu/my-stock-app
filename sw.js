75e738c8f6d25fe67ea6451911a54a7aa977efa8
comuseliu <comuseliu@gmail.com>
2026-04-17 14:56:17 +0800

    Initial commit
    Initial commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'stock-v1';
+const ASSETS = [
+  './存股紀錄_單人版_月張數.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
